ed9c0b4768f8c256b55cffbab45c5b317f4a97bb
yoeoh <yoeooh@gmail.com>
2023-02-25 03:16:23 +0300

    created sticky compact header with intersection observer

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // select
   NiceSelect.bind(document.getElementById("order-select-dropdown"));
 
+  // scrollbar for select
   new SimpleBar(document.querySelector(".nice-select .list"), {
     autoHide: false,
   });
 
+  // range
   const rangeInput = document.getElementById("form-range-input");
   const rangeValue = document.getElementById("form-range-value");
 
@@ -12,4 +15,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const value = e.target.value;
     rangeValue.innerText = `${value}%`;
   });
+
+  // sticky header with changing style
+  const header = document.getElementById("header");
+  const hero = document.getElementById("hero");
+
+  const options = {
+    threshold: 1,
+  };
+
+  const heroObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        header.classList.add("header__compact");
+      } else {
+        header.classList.remove("header__compact");
+      }
+    });
+  }, options);
+
+  heroObserver.observe(hero);
 });
