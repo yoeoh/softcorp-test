789dbc6c4bbabca9735f33b3cec769486dd9e33d
yoeoh <yoeooh@gmail.com>
2023-02-27 01:16:03 +0300

    js concat and minify, gulpfile and package.json cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,13 @@
 import gulp from "gulp";
 const { src, dest, watch, series } = gulp;
 
-import webpackStream from "webpack-stream";
-import webpack from "webpack";
-import TerserPlugin from "terser-webpack-plugin";
-
 import imagemin, { gifsicle, mozjpeg, optipng, svgo } from "gulp-imagemin";
 
 import prefix from "gulp-autoprefixer";
 import minify from "gulp-clean-css";
 import concat from "gulp-concat";
+
+import terser from "gulp-terser";
 
 import gulpSass from "gulp-sass";
 import defaultSass from "sass";
@@ -21,29 +19,22 @@ function copyModulesJs() {
     "node_modules/simplebar/dist/simplebar.min.js",
   ];
 
-  return src(modules).pipe(dest("src/scripts"));
+  return src(modules).pipe(dest("src/scripts/libs"));
 }
 
 function copyModulesCss() {
-  const modules = [
-    // "node_modules/nice-select2/dist/css/nice-select2.css",
-    "node_modules/simplebar/dist/simplebar.min.css",
-  ];
+  const modules = ["node_modules/simplebar/dist/simplebar.min.css"];
 
   return src(modules).pipe(dest("dist/css"));
 }
 
-// function copyModulesScss() {
-//   const modules = ["node_modules/nice-select2/src/scss/nice-select2.scss"];
-
-//   return src(modules).pipe(dest("src/scss"));
-// }
-
 function buildScripts() {
-  return src("src/scripts/*.js").pipe(dest("dist/scripts"));
+  return src(["src/scripts/libs/*.js", "src/scripts/*.js"])
+    .pipe(concat("app.js"))
+    .pipe(terser())
+    .pipe(dest("dist/scripts"));
 }
 
-// scss
 function compileCss() {
   return src(["src/scss/*.scss"])
     .pipe(sass.sync())
@@ -52,7 +43,6 @@ function compileCss() {
     .pipe(dest("dist/css"));
 }
 
-// images
 function optimizeImg() {
   return src("src/assets/images/*.{jpg,png,ico,svg}")
     .pipe(
@@ -65,17 +55,14 @@ function optimizeImg() {
     .pipe(dest("dist/images"));
 }
 
-// copy html
 function buildHtml() {
   return src("index.html").pipe(dest("dist"));
 }
 
-// copy fonts
 function copyFonts() {
   return src("src/assets/fonts/*").pipe(dest("dist/fonts"));
 }
 
-// watch
 function watchTask() {
   watch("src/scss/*.scss", compileCss);
   watch("src/assets/images/*.{jpg,png}", optimizeImg);
@@ -86,7 +73,6 @@ function watchTask() {
 export default series(
   copyModulesJs,
   copyModulesCss,
-  // copyModulesScss,
   buildScripts,
   compileCss,
   optimizeImg,
