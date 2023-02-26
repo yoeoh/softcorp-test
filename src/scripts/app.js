e03abf9767d4b3ad6e0ddb4967f729c5b4738e04
yoeoh <yoeooh@gmail.com>
2023-02-26 05:19:18 +0300

    burger menu first iteration

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const options = {
     threshold: 1,
+    // rootMargin: "",
   };
 
   const heroObserver = new IntersectionObserver((entries) => {
@@ -35,4 +36,46 @@ document.addEventListener("DOMContentLoaded", () => {
   }, options);
 
   heroObserver.observe(hero);
+
+  // burger menu with copy of main menu
+  const burgerButton = document.getElementById("burger-menu-button");
+  const burgerMenu = document.getElementById("burger-menu");
+  const burgerCloseButton = document.getElementById("burger-menu-close-button");
+  const headerMenu = document.getElementById("header-menu");
+  const body = document.querySelector("body");
+
+  function openBurgerMenu() {
+    burgerMenu.classList.add("burger_visible");
+    copyMenu();
+
+    body.classList.add("fullscreen");
+  }
+
+  function closeBurgerMenu() {
+    burgerMenu.classList.remove("burger_visible");
+    body.classList.remove("fullscreen");
+  }
+
+  let isCopied = false;
+
+  function copyMenu() {
+    if (isCopied) return;
+
+    isCopied = true;
+
+    const menuItemsArray = Array.from(
+      headerMenu.cloneNode(true).getElementsByTagName("li")
+    );
+
+    const list = document.getElementById("burger-menu-list");
+
+    menuItemsArray.forEach((item) => {
+      list.appendChild(item);
+    });
+
+    burgerMenu.addEventListener("click", () => closeBurgerMenu());
+  }
+
+  burgerButton.addEventListener("click", () => openBurgerMenu());
+  burgerCloseButton.addEventListener("click", () => closeBurgerMenu());
 });
